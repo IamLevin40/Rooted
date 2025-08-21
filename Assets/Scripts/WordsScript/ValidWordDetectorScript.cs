2adbc6104fb511cf3ecc2ffd728031c76f07aad2
IamLevin40 <levincabales40@gmail.com>
2025-08-22 00:07:41 +0800

    feat: preparation for word detector; txt word files

---
+++ Assets/Scripts/WordsScript/ValidWordDetectorScript.cs
@@ -0,0 +1,25 @@
+// Input is the word to be checked, which was made from WordBuildingScript.cs
+// Must detect and return the following:
+// (a.) Return True if the word is valid, False if not
+//      Base this on the text file "valid_words.txt" under Resources/ExternalFiles
+// (b.) Return the meaning if the word is valid
+//      Base this on the csv file "glossary.csv" under Resources/ExternalFiles
+// (c.) Return True if the word is environmentally-related, False if not.
+//      Base this on the text file "environmental_words.txt" under Resources/ExternalFiles
+
+using UnityEngine;
+
+public class ValidWordDetectorScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
